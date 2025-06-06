8387ae9bf689f9d0bf41a016410eb19b6c227e52
prathamlahoti123 <prathamlahoti@duck.com>
2025-06-06 17:50:50 +0300

    Create a FastAPI app with /health endpoint

---
+++ app/api/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, status
+
+from api.routes import router
+
+
+app = FastAPI()
+app.include_router(router)
+
+
+@app.get(
+  "/health",
+  description="Healthcheck of the API",
+  responses={
+    status.HTTP_200_OK: {
+      "description": "Healthcheck status",
+      "content": {
+        "application/json": {
+          "example": {"response": "ok"}
+        }
+      }
+    }
+  }
+)
+async def health() -> dict[str, str]:
+  """Healthcheck endpoint."""
+  return {"response": "ok"}
