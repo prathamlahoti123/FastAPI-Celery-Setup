ad9d91a393fc79a9892ec57023c5a1271ed41453
prathamlahoti123 <prathamlahoti@duck.com>
2025-06-06 17:52:32 +0300

    Add route to run a celery task

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,8 @@
 from celery.result import AsyncResult
 from fastapi import APIRouter
 
-from api.schemas import Task
+from api.schemas import Task, TaskID
+from api.tasks import some_complex_task
 
 
 router = APIRouter()
@@ -16,3 +17,14 @@ def get_task_status(id: str) -> Task:
   """Get info about celery task"""
   res = AsyncResult(id)
   return Task(id=id, status=res.status, result=res.result)
+
+
+@router.post(
+  "/tasks/run",
+  description="Run a background task",
+  response_model=TaskID
+)
+async def run_task(a: int, b: int) -> TaskID:
+  """Endpoint to run a 'CPU-intensive' background task"""
+  task: AsyncResult = some_complex_task.delay(a, b)
+  return TaskID(id=task.id)
