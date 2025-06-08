40b35e0a4e7a43b1283a2a5e8a76cf68fced5dc8
prathamlahoti123 <prathamlahoti@duck.com>
2025-06-08 13:17:31 +0300

    Add test to send a task to the queue

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,15 +1,29 @@
 from typing import TYPE_CHECKING
+from uuid import UUID
 
 import pytest
 from fastapi import status
 
+from api.tasks import some_complex_task
+
 if TYPE_CHECKING:
-  from httpx import AsyncClient
+  from celery.result import AsyncResult
+  from fastapi.testclient import TestClient
 
 
-@pytest.mark.anyio
-async def test_health(client: "AsyncClient") -> None:
+def test_health(client: "TestClient") -> None:
   """Test healthcheck endpoint."""
-  resp = await client.get("/health")
+  resp = client.get("/health")
   assert resp.status_code == status.HTTP_200_OK
   assert resp.json() == {"response": "ok"}
+
+
+def test_run_task() -> None:
+  """Test sending a task to the celery queue."""
+  res: AsyncResult = some_complex_task.delay(2, 2)
+  assert res.result is None
+  assert res.status == "PENDING"
+  try:
+    UUID(res.id)
+  except ValueError:
+    pytest.fail("Task ID is not a valid UUID")
