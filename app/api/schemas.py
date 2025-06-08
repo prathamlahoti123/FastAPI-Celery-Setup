75514efd9b5ca06a635793e95cca774c652701ac
prathamlahoti123 <prathamlahoti@duck.com>
2025-06-08 13:10:21 +0300

    Update docstrings

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel
 
 
 class TaskID(BaseModel):
-  """Schema to represent ID of a celery task"""
+  """Schema to represent ID of a celery task."""
   id: str
 
 
 class Task(TaskID):
-  """Schema to represent info about celery task"""
+  """Schema to represent info about celery task."""
   status: str
   result: float | None = None # bg task returns float as a result
