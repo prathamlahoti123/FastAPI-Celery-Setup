960bfbc9dbb3fefde834d07926f031991fb2c1f0
prathamlahoti123 <prathamlahoti@duck.com>
2025-06-08 13:18:21 +0300

    Fix Ruff errors

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -2,5 +2,5 @@ from celery import shared_task
 
 
 @shared_task(name="some-complex-task")
-def some_complex_task(*args, **kwargs) -> None:
-  """'CPU-intensive' task to be processed by Celery worker"""
+def some_complex_task(*args: int, **kwargs: int) -> None:
+  """'CPU-intensive' task to be processed by Celery worker."""
