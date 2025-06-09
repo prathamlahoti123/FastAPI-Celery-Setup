43ee14f7448b5d83c050df1f6153411fb091be52
prathamlahoti123 <prathamlahoti@duck.com>
2025-06-09 12:36:32 +0300

    Separate task and its business logic function

--- worker/app/tasks.py
+++ worker/app/tasks.py
@@ -1,27 +1,36 @@
-import time
-
-from celery.utils.log import get_task_logger
+from typing import TYPE_CHECKING
 
 from app.main import celery
 
+if TYPE_CHECKING:
+  from celery.app.task import Task
 
-logger = get_task_logger(__name__)
+
+def div(a: int, b: int) -> float:
+  """Perform division of 2 numbers and return the result.
+
+  NOTE: this function contains some business logic to be
+        executed within a background task.
+  """
+  return a / b
 
 
 @celery.task(
   name="some-complex-task",
   bind=True,
   max_retries=2,
-  default_retry_delay=10,
+  default_retry_delay=5,
 )
-def some_complex_task(self, a: int, b: int) -> float | None:
-  """'CPU-intensive' task to calculate a complex math operation"""
-  logger.info("Running the background task ...")
+def some_complex_task(self: "Task", a: int, b: int) -> float:
+  """Run a 'CPU-intensive' math operation in the background task.
+
+  NOTE (1): the business logic and the actual task are separated
+            in order to test them as different units.
+
+  NOTE (2): retry policy is strictly used for demonstration purposes.
+  """
   try:
-    res = a / b
+    res = div(a, b)
   except ZeroDivisionError as exc:
-    logger.error(str(exc))
-    self.retry(exc=exc)
-  # simulate processing ...
-  time.sleep(10)
+    raise self.retry(exc=exc)  # noqa: B904
   return res
