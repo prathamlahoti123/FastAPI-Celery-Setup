79f6140c93be2b3d7fa378597b064dc8129fe5dd
prathamlahoti123 <prathamlahoti@duck.com>
2025-06-12 12:46:15 +0300

    Fix wrong name of the env variable

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -8,7 +8,7 @@ from api.main import app
 
 
 # e2e mode should be enabled when running e2e tests
-E2E_MODE_DISABLED = bool(int(os.getenv("E2E_MODE_ENABLED", "1")))
+E2E_MODE_DISABLED = bool(int(os.getenv("E2E_MODE_DISABLED", "1")))
 if E2E_MODE_DISABLED:
   os.environ["CELERY_BROKER_URL"] = "memory://"
   os.environ["CELERY_RESULT_BACKEND"] = "rpc://"
