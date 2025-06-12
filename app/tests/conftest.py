c4cadbd38be3a86a6800312ff302212d69f64fff
prathamlahoti123 <prathamlahoti@duck.com>
2025-06-12 23:48:46 +0300

    Add E2E client

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,21 +1,21 @@
-import os  # noqa: I001
 from collections.abc import Iterator
 
 import pytest
 from fastapi.testclient import TestClient
+from httpx import Client
 
 from api.main import app
-
-
-# e2e mode should be enabled when running e2e tests
-E2E_MODE_DISABLED = bool(int(os.getenv("E2E_MODE_DISABLED", "1")))
-if E2E_MODE_DISABLED:
-  os.environ["CELERY_BROKER_URL"] = "memory://"
-  os.environ["CELERY_RESULT_BACKEND"] = "rpc://"
 
 
 @pytest.fixture(scope="session")
 def client() -> Iterator[TestClient]:
   """HTTP client to test FastAPI endpoints."""
   with TestClient(app) as cl:
+    yield cl
+
+
+@pytest.fixture(scope="session")
+def e2e_client() -> Iterator[Client]:
+  """HTTP client to test FastAPI endpoints in E2E mode."""
+  with Client(base_url="http://localhost:8000") as cl:
     yield cl
