de77eab34fae77305b41f5fb1cb6a88ba824ed37
prathamlahoti123 <prathamlahoti@duck.com>
2025-06-12 23:50:14 +0300

    Add e2e tests

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,15 +1,21 @@
+import os
+import time
 from typing import TYPE_CHECKING
-from uuid import UUID
 
 import pytest
 from fastapi import status
 
+from api.schemas import TaskData
 from api.tasks import some_complex_task
-from tests.conftest import E2E_MODE_DISABLED
 
 if TYPE_CHECKING:
   from celery.result import AsyncResult
   from fastapi.testclient import TestClient
+  from httpx import Client
+
+
+# End-to-end mode should be enabled when running end-to-end tests
+E2E_MODE_DISABLED = bool(int(os.getenv("E2E_MODE_DISABLED", "1")))
 
 
 def test_health(client: "TestClient") -> None:
@@ -19,17 +25,70 @@ def test_health(client: "TestClient") -> None:
   assert resp.json() == {"response": "ok"}
 
 
-def test_run_task() -> None:
+@pytest.mark.parametrize(
+  "task_data",
+  [TaskData(a=2, b=2)]
+)
+def test_run_task(task_data: TaskData) -> None:
   """Test sending a task to the celery queue."""
-  res: AsyncResult = some_complex_task.delay(2, 2)
+  os.environ["CELERY_BROKER_URL"] = "memory://"
+  os.environ["CELERY_RESULT_BACKEND"] = "rpc://"
+
+  res: AsyncResult = some_complex_task.delay(task_data.a, task_data.b)
   assert res.result is None
   assert res.status == "PENDING"
-  try:
-    UUID(res.id)
-  except ValueError:
-    pytest.fail("Task ID is not a valid UUID")
+  assert res.id
 
 
+@pytest.mark.parametrize(
+  ("task_data", "result"),
+  [
+    (TaskData(a=2, b=2), 1.0),
+    (TaskData(a=4, b=2), 2.0)
+  ]
+)
 @pytest.mark.skipif(E2E_MODE_DISABLED, reason="E2E mode disabled")
-def test_e2e_run_task() -> None:
+def test_e2e_run_task(e2e_client: "Client", task_data: TaskData, result: float) -> None:
   """E2E test to run the task with a real broker and worker."""
+  run_task_res = e2e_client.post(
+    "/tasks/run",
+    json=task_data.model_dump()
+  )
+  assert run_task_res.status_code == status.HTTP_200_OK
+  assert "id" in run_task_res.json()
+  task_id = run_task_res.json()["id"]
+
+  # Add delay to make sure the task is finished
+  time.sleep(0.5)
+
+  # Get info about task
+  get_task_res = e2e_client.get(f"/tasks/{task_id}")
+  assert get_task_res.status_code == status.HTTP_200_OK
+  task_info = get_task_res.json()
+  assert task_info["id"] == task_id
+  assert task_info["status"] == "SUCCESS"
+  assert task_info["result"] == result
+
+
+@pytest.mark.parametrize(
+  "task_data",
+  [TaskData(a=2, b=0)]
+)
+@pytest.mark.skipif(E2E_MODE_DISABLED, reason="E2E mode disabled")
+def test_e2e_run_task_with_error(e2e_client: "Client", task_data: TaskData) -> None:
+  """E2E test to run the task with a real broker and worker."""
+  run_task_res = e2e_client.post(
+    "/tasks/run",
+    json=task_data.model_dump()
+  )
+  assert run_task_res.status_code == status.HTTP_200_OK
+  assert "id" in run_task_res.json()
+  task_id = run_task_res.json()["id"]
+
+  # Get info about task
+  get_task_res = e2e_client.get(f"/tasks/{task_id}")
+  assert get_task_res.status_code == status.HTTP_200_OK
+  task_info = get_task_res.json()
+  assert task_info["id"] == task_id
+  assert task_info["status"] == "RETRY"
+  assert task_info["result"] is None
