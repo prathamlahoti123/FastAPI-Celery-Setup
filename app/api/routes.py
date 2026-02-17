156ef23c15f77495d3434bd3285073798fcfb1e1
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-17 12:54:40 +0200

    Remove redundant description for each endpoint

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,28 +1,21 @@
-from celery.result import AsyncResult  # noqa: I001
+from celery.result import AsyncResult
 from fastapi import APIRouter
 
-from api.schemas import Task, TaskID, TaskData
+from api.schemas import Task, TaskData, TaskID
 from api.tasks import some_complex_task
-
 
 router = APIRouter()
 
 
-@router.get(
-  "/tasks/{task_id}",
-  description="Status and result of a celery task",
-)
+@router.get("/tasks/{task_id}")
 def get_task_status(task_id: str) -> Task:
-  """Get info about celery task."""
+  """Return status and result of the celery task."""
   res = AsyncResult(task_id)
   return Task(id=res.id, status=res.status, result=res.result)
 
 
-@router.post(
-  "/tasks/run",
-  description="Run a background task",
-)
+@router.post("/tasks/run")
 def run_task(data: TaskData) -> TaskID:
-  """Endpoint to run a 'CPU-intensive' background task."""
+  """Run a background task to be executed in a separate worker process."""
   task: AsyncResult = some_complex_task.delay(data.a, data.b)
   return TaskID(id=task.id)
