8be9ac834de7d439b1cd03067154d1665d39e8fc
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-18 22:07:00 +0200

    Update docstring

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -3,4 +3,11 @@ from celery import shared_task
 
 @shared_task(name="some-complex-task")
 def some_complex_task(*args: int, **kwargs: int) -> None:
-  """'CPU-intensive' task to be processed by Celery worker."""
+  """'CPU-intensive' task to be processed by Celery worker.
+
+  Optional number of retries and delay between retries
+  can be passed as keyword arguments. For example:
+    task = some_complex_task.delay(data.a, data.b, retry_attempts=6, retry_delay=10)
+
+  If not passed, default values defined by the worker will be used.
+  """
