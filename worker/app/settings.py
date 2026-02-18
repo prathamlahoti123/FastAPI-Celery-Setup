f9952ac16d893a38d7b4e494222414ab24e57b61
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-18 22:09:29 +0200

    Add settings to specify retry attempts and delay for failed task

--- worker/app/settings.py
+++ worker/app/settings.py
@@ -2,3 +2,5 @@ import os
 
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/1")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/2")
+CELERY_RETRY_DELAY = int(os.getenv("CELERY_RETRY_DELAY", "1"))
+CELERY_MAX_RETRIES = int(os.getenv("CELERY_MAX_RETRIES", "3"))
