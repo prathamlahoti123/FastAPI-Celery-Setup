e38ff7a64cf8c28b0b79dd0731a42a12df77be4d
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-18 09:54:25 +0200

    Fix mypy errors

--- app/api/routes.py
+++ app/api/routes.py
@@ -10,12 +10,13 @@ router = APIRouter()
 @router.get("/tasks/{task_id}")
 def get_task_status(task_id: str) -> Task:
   """Return status and result of the celery task."""
-  res = AsyncResult(task_id)
-  return Task(id=res.id, status=res.status, result=res.result)
+  task: AsyncResult[float | None] = AsyncResult(task_id)
+  result = None if isinstance(task.result, BaseException) else task.result
+  return Task(id=task.id, status=task.status, result=result)
 
 
 @router.post("/tasks/run")
 def run_task(data: TaskData) -> TaskID:
   """Run a background task to be executed in a separate worker process."""
-  task: AsyncResult = some_complex_task.delay(data.a, data.b)
+  task = some_complex_task.delay(data.a, data.b)
   return TaskID(id=task.id)
