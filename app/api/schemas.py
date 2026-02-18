44e3b20df8d9b2bf709461c8e273852e1d947b37
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-18 09:53:43 +0200

    Fix mypy errors

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,19 +1,23 @@
+from typing import Any
+
 from pydantic import BaseModel, model_validator
 
 
 class TaskID(BaseModel):
   """Schema to represent ID of a celery task."""
+
   id: str
 
 
 class Task(TaskID):
   """Schema to represent info about celery task."""
+
   status: str
-  result: float | None  = None
+  result: float | None = None
 
   @model_validator(mode="before")
   @classmethod
-  def validate_result(cls, data: dict) -> dict:
+  def validate_result(cls, data: dict[str, Any]) -> dict[str, Any]:
     """Validate result of the task based on its status."""
     if data["status"] != "SUCCESS":
       data["result"] = None
@@ -22,5 +26,6 @@ class Task(TaskID):
 
 class TaskData(BaseModel):
   """Schema to represent data to be processed by a task."""
+
   a: int = 1
   b: int = 1
